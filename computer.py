8a589bbccffa81adb3a1c0c1665382c2215dd650
Jeremy Le <jeremyle@live.com>
2021-10-09 21:59:24 +1100

    implement ai

--- computer.py
+++ computer.py
@@ -1,43 +1,37 @@
-# TODO: 1. Count the number of 0's, 1's, 2's, etc., in your hand, and in the snake.
-#       2. Each domino in your hand receives a score equal to the sum of appearances of each of its numbers.
 from domino import Domino
 from hand import Hand
 from snake import Snake
 
-hand = Hand([Domino(1,5), Domino(3,5), Domino(0,5)])
-# Hand([Domino(i, i) for i in range(7)])
-snake = Snake()
-snake.add_list([Domino(4,4), Domino(4,2), Domino(2,1), Domino(1,0), Domino(0,0), Domino(0,2)])
-    # [Domino(i, i+1) for i in range(4)])
 
 class Computer:
     def __init__(self):
-        self.hand = self.snake = self._counts = self._values = self._counts = None
+        self.hand = (
+            self.snake
+        ) = self._counts = self._values = self.commands = self._counts = None
 
-
-    def _flatten(self, hand):
-        """Flattens/unpacks a list of dominos into a list of pips
+    def _flat(self, hand) -> tuple:
+        """Flattens the hand by concatenating items in a list altogether into a single list.
         e.g: [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
         """
-        return sum((_.as_list() for _ in hand.as_list()), [])  #  flattens dominoes
+        return sum((_.as_tuple() for _ in hand.as_tuple()), ())  #  concatenates
 
-    def _counting(self, flat) -> dict:
-        """Counts the number of items it appears in a flattened list."""        
-        return {i: flat.count(i) for i in [0,1,2,3,4,5,6]}
+    def _count(self, flat: tuple) -> dict:
+        """Counts the number of items it appears in a flattened list."""
+        return {_: flat.count(_) for _ in (0, 1, 2, 3, 4, 5, 6)}
 
-    def _scoring(self, count, list) -> list:
-        return [sum(count.get(j) for j in i) for i in list]
+    def _value(self, count: dict, hand: Hand) -> list:
+        return [sum(count.get(j) for j in i) for i in hand.as_tuple()]
 
-    def score_command(self, hand, snake):
+    def scoring(self, hand, snake):
+        """computer hand must be first argument, followed by snake
+
+        Returns: list
+            returns sorted list of most to least valuable commands
+        """
         self.hand = hand
         self.snake = snake
-        self._counts = self._counting(self._flatten(self.hand) + self._flatten(self.snake))
-        self._values = self._scoring(self._counts, self.hand.as_list())
-        commands = [i+1 for i in range(len(self.hand))]
-        self._scores = dict(zip(commands, self._values))
+        self._counts = self._count(self._flat(self.hand) + self._flat(self.snake))
+        self._values = self._value(self._counts, self.hand)
+        self.commands = [i + 1 for i in range(len(self.hand))]
+        self._scores = sorted(list(zip(self._values, self.commands)), reverse=True)
         return self._scores
-
-
-
-
-ai = Computer()
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from domino import Domino
 from snake import Snake
 from hand import Hand
 from boneyard import BoneYard
-from random import sample, randint
+from random import sample
+from computer import Computer
 
 
 def create_set(Double: bool = False):
@@ -13,19 +14,21 @@ def create_set(Double: bool = False):
         return sample([Domino(i, j) for i in range(7) for j in range(i, 7)], 28)
 
 
-class Game():
+class Game:
     def __init__(self):
 
-        self.status = ''
-        self.ai = self.ai_select = self.ai_tile = self.select = self.play_tile = None
+        self.status = ""
+        self.ai_list = self.select = self.play_tile = None
         self._deck = self.create_deck()
         self._initial = self.high_double
         self.boneyard = self.make_boneyard()
         self.player = self.deal_player()
+        self.ai = Computer()
         self.computer = self.deal_computer()
         self.remove_initial()
         self.snake = self.create_snake()
-####################################  INIT METHODS  ####################################
+
+    ####################################  INIT METHODS  ####################################
 
     @property
     def high_double(self):
@@ -46,33 +49,37 @@ class Game():
     def remove_initial(self):
         if self.high_double in self.player:
             self.player._remove(self.high_double)
-            self.status = 'computer'
+            self.status = "computer"
         elif self.high_double in self.computer:
             self.computer._remove(self.high_double)
-            self.status = 'player'
+            self.status = "player"
 
     def create_snake(self):
         return Snake(self.high_double)
 
-##################################   GAME LOGIC   ######################################
+    ##################################   GAME LOGIC   ######################################
 
     def command(self):
-        if self.status == 'computer':
+        if self.status == "computer":
             # Press Enter -> perform computer action -> switch status
             input(
-                "Status: The computer is about to make a move. Press Enter to continue.")
-            self.computer_action(self.computer_ai())
-            self.status = 'player'
-        elif self.status == 'player':
+                "Status: The computer is about to make a move. Press Enter to continue."
+            )
+            self.computer_ai()
+            self.status = "player"
+        elif self.status == "player":
             #  Enter command -> perform player action -> switch status
             self.player_action(self.player_prompt())
-            self.status = 'computer'
+            self.status = "computer"
 
     def player_prompt(self) -> int:
         while True:
             try:
                 command = int(
-                    input("Status: It's your turn to make a move. Enter your command.\n"))
+                    input(
+                        "Status: It's your turn to make a move. Enter your command.\n"
+                    )
+                )
             except ValueError:
                 print("Invalid input. Please try again.")
             else:
@@ -95,7 +102,7 @@ class Game():
 
     def player_action(self, command):
         if command != 0:
-            self.select = abs(command)-1
+            self.select = abs(command) - 1
             if not self.legal_check(command):
                 self.player_action(self.illegal_prompt())
 
@@ -107,76 +114,78 @@ class Game():
         elif len(self.boneyard) > 0:
             self.player.draw(self.boneyard.draw())
 
-
     def computer_ai(self):
-        self.ai = randint(-1 * len(self.computer), len(self.computer))
-        return self.ai
+        self.ai_list = self.ai.scoring(self.computer, self.snake)
+        for ai_ in self.ai_list:
+            self.ai_monitor = self.computer[abs(ai_[1]) - 1]
+            ai_r = ai_[1]
+            ai_l = -1 * ai_[1]
+            if self.legal_check(ai_r):
+                self.snake.add_right(self.flip_tile(ai_r))
+                break
+            elif self.legal_check(ai_l):
+                self.snake.add_left((self.flip_tile(ai_l)))
+                break
+            else:
+                continue
+        else:
+            if len(self.boneyard) > 0:
+                self.computer.draw(self.boneyard.draw())
 
-    def computer_action(self, command):
-        if command != 0:
-            self.ai_select = abs(command)-1
-            if not self.legal_check(command):
-                self.computer_action(self.computer_ai())
-                
-            elif self.ai > 0:
-                self.snake.add_right(self.flip_tile(command))
-            elif self.ai < 0:
-                self.snake.add_left(self.flip_tile(command))
-
-        elif len(self.boneyard) > 0:
-            self.computer.draw(self.boneyard.draw())
-
-##################################   GAME RULES   ######################################
+    ##################################   GAME RULES   ######################################
 
     def legal_check(self, command):
         """At this point, only checks legality of piece but does not play yet (remove from hand)"""
-        if self.status == 'player':
+        if self.status == "player":
             if command > 0:
-                # only indexes domino
                 return self.snake[-1].right in self.player[self.select]
             elif command < 0:
                 return self.snake[0].left in self.player[self.select]
-        elif self.status == 'computer':
+
+        elif self.status == "computer":
             if command > 0:
-                return self.snake[-1].right in self.computer[self.ai_select]
+                return self.snake[-1].right in self.computer[command - 1]
             elif command < 0:
-                return self.snake[0].left in self.computer[self.ai_select]
+                return self.snake[0].left in self.computer[abs(command) - 1]
 
     def flip_tile(self, command):
         """Legal piece, therefore flip if required"""
-        if self.status == 'player':
+        if self.status == "player":
             # This removes the piece from hand
             self.play_tile = self.player.play(self.select)
             if command > 0:
-                if self.snake[-1].right != self.play_tile.left:
-                    return self.play_tile.flip()
-                else:
-                    return self.play_tile
+                return (
+                    self.play_tile
+                    if self.snake[-1].right == self.play_tile.left
+                    else self.play_tile.flip()
+                )
             elif command < 0:
-                if self.snake[0].left != self.play_tile.right:
-                    return self.play_tile.flip()
-                else:
-                    return self.play_tile
+                return (
+                    self.play_tile
+                    if self.snake[0].left == self.play_tile.right
+                    else self.play_tile.flip()
+                )
 
-        elif self.status == 'computer':
-            self.ai_tile = self.computer.play(self.ai_select)
+        elif self.status == "computer":
+            self.ai_tile = self.computer.play(abs(command) - 1)
             if command > 0:
-                if self.snake[-1].right != self.ai_tile.left:
-                    return self.ai_tile.flip()
-                else:
-                    return self.ai_tile
+                return (
+                    self.ai_tile
+                    if self.snake[-1].right == self.ai_tile.left
+                    else self.ai_tile.flip()
+                )
             elif command < 0:
-                if self.snake[0].left != self.ai_tile.right:
-                    return self.ai_tile.flip()
-                else:
-                    return self.ai_tile
+                return (
+                    self.ai_tile
+                    if self.snake[0].left == self.ai_tile.right
+                    else self.ai_tile.flip()
+                )
 
-
-######################################  DISPLAY  #######################################
+    ######################################  DISPLAY  #######################################
 
     def list_pieces(self):
         for i in range(len(self.player)):
-            print(f'{i + 1}:{self.player.hand[i]}')
+            print(f"{i + 1}:{self.player.hand[i]}")
 
     def screen(self):
         """Displays the current updated screen"""
@@ -186,10 +195,10 @@ class Game():
         print()
         print(self.snake)
         print()
-        print('Your pieces:')
+        print("Your pieces:")
         self.list_pieces()
 
-########################################################################################
+    ########################################################################################
 
     def draw_condition(self) -> bool:
         """Checks if both ends of snake are the same and that there are at least 8
@@ -197,15 +206,16 @@ class Game():
         if self.snake[0].left != self.snake[-1].right:
             return False
 
-        all_pips = [self.snake[_].left for _ in range(len(self.snake))] + \
-            [self.snake[_].right for _ in range(len(self.snake))]
+        all_pips = [self.snake[_].left for _ in range(len(self.snake))] + [
+            self.snake[_].right for _ in range(len(self.snake))
+        ]
         return all_pips.count(self.snake[0].left) == 8
 
     def empty_hand(self) -> bool:
         """Checks if either hands are empty"""
         return len(self.computer) == 0 or len(self.player) == 0
 
-########################################################################################
+    ########################################################################################
 
     def play(self):
         while not self.draw_condition() and not self.empty_hand():
