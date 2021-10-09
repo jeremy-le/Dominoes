7e2120c57548a28dd513e96fb2c577d517ac9951
Jeremy Le <jeremyle@live.com>
2021-10-09 19:30:22 +1100

    refactored in methods

--- computer.py
+++ computer.py
@@ -11,40 +11,33 @@ snake.add_list([Domino(4,4), Domino(4,2), Domino(2,1), Domino(1,0), Domino(0,0),
     # [Domino(i, i+1) for i in range(4)])
 
 class Computer:
-    def __init__(self, hand, snake):
-        self.hand = hand
-        self.snake = snake
-        self.hand_list = self.hand.as_list()
-        self._counts = self._counting(self._flatten(self.hand) + self._flatten(self.snake))
-        self._values = self._scoring(self._counts, self.hand_list)
-        self._scores = dict(zip(self.hand, self._values))
+    def __init__(self):
+        self.hand = self.snake = self._counts = self._values = self._counts = None
+
 
     def _flatten(self, hand):
         """Flattens/unpacks a list of dominos into a list of pips
         e.g: [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
-
-        Args:
-            hand (list): either hand or snake list of domino objects
-
-        Returns:
-            list: returns a flattened list of all integer values
         """
         return sum((_.as_list() for _ in hand.as_list()), [])  #  flattens dominoes
 
     def _counting(self, flat) -> dict:
-        """Counts the number of items it appears in a flattened list.
-
-        Returns:
-            dict: key 1 - 6: count of key occurences
-        """        
+        """Counts the number of items it appears in a flattened list."""        
         return {i: flat.count(i) for i in [0,1,2,3,4,5,6]}
 
     def _scoring(self, count, list) -> list:
         return [sum(count.get(j) for j in i) for i in list]
 
-    def get_scores(self):
+    def score_command(self, hand, snake):
+        self.hand = hand
+        self.snake = snake
+        self._counts = self._counting(self._flatten(self.hand) + self._flatten(self.snake))
+        self._values = self._scoring(self._counts, self.hand.as_list())
+        commands = [i+1 for i in range(len(self.hand))]
+        self._scores = dict(zip(commands, self._values))
         return self._scores
 
 
 
-ai = Computer(hand,snake)
+
+ai = Computer()
